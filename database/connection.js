f28dc30419b289634cc4d5edd6b10d601046a8de
ayushsoni11 <soniayush6441@gmail.com>
2025-03-31 12:38:28 +0530

    Updated mongodb string

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 
 export const connection = ()=>{
-    mongoose.connect( 'mongodb://127.0.0.1:27017' , {
+    mongoose.connect( 'mongodb+srv://ayushsoni:wGV1Bai0uE1FeBNE@cluster0.vcjsavt.mongodb.net/' , {
         dbName : "SYNCORA_TWO",
     }).then(()=>{
         console.log("Database connected")
